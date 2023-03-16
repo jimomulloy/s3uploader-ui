0a39366a9aa916bf4bee11b342088e03eca2384d
Jim O'Mulloy <jimomulloy@gmail.com>
2023-03-16 16:50:10 +0000

    initial build for instrument S3 uploader

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,36 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "instrumentuploader1fed0e2b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger4bea66b2": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "hosting": {
+    "S3AndCloudFront": {
+      "HostingBucketName": "string",
+      "Region": "string",
+      "S3BucketSecureURL": "string",
+      "WebsiteURL": "string"
+    }
+  },
+  "storage": {
+    "instrument": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
